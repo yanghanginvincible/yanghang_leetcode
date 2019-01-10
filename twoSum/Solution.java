1a1c36ef72f89f48f1479549b20677ac91f3b0fa
yanghanginvincible <1214367289@qq.com>
2019-01-10 21:20:10 +0800

    twoSum

---
+++ twoSum/Solution.java
@@ -0,0 +1,17 @@
+﻿class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i;
+        int j;
+        int[] a = new int[2]; 
+        loop1:for(i=0;i<(nums.length-1);i++){
+            for(j=i+1;j<nums.length;j++){
+                if((nums[i]+nums[j])==target){
+                    a[0]=i;
+                    a[1]=j;
+                    break loop1;
+                }
+            }
+        }
+    return a;
+    }
+}
