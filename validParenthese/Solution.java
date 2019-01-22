c27ba88a863eb0ea5d49fbfe60e49ef4760313e9
yanghanginvincible <1214367289@qq.com>
2019-01-22 20:57:29 +0800

    validParenthese

---
+++ validParenthese/Solution.java
@@ -0,0 +1,14 @@
+﻿class Solution {
+    public boolean isValid(String s) {
+        boolean b = false;
+        int j = s.length()/2;
+        if(s.length()==0)return b=true;
+        for(int i = 0;i<j;i++){
+            s = s.replace("[]","");
+            s = s.replace("{}","");
+            s = s.replace("()","");
+        }
+        b=(s.length()==0)?true:false;
+        return b;
+    }
+}
